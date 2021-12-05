777760e787d4db72f20c954dfaad4a583c1b2fd2
gcirit <gcirit@gmail.com>
2021-12-05 17:36:59 +0300

    Initial Commit

---
+++ src/BigIntegerSolution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class BigIntegerSolution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger a = in.nextBigInteger();
+        BigInteger b = in.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
